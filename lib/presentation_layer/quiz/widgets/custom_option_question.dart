2bb7ee0e0bef7f8274bb36f70e7cb32f23e191b4
AliHassanAli <alihassenali908@gmail.com>
2025-08-14 00:15:18 +0300

    finish the ui quiz

--- lib/presentation_layer/quiz/widgets/custom_option_question.dart
+++ lib/presentation_layer/quiz/widgets/custom_option_question.dart
@@ -4,7 +4,7 @@ import 'package:online_exam/core/utils/app_colors.dart';
 
 class CustomOptionsQuestion extends StatefulWidget {
   final List<String> options;
-  final Function(List<int>) onChanged;
+  final ValueChanged<int> onChanged;
 
   const CustomOptionsQuestion({
     super.key,
@@ -17,60 +17,52 @@ class CustomOptionsQuestion extends StatefulWidget {
 }
 
 class _CustomOptionsQuestionState extends State<CustomOptionsQuestion> {
-  List<int> selectedIndexes = [];
+  int? selectedIndex;
 
-  void toggleOption(int index) {
+  void selectOption(int index) {
     setState(() {
-      if (selectedIndexes.contains(index)) {
-        selectedIndexes.remove(index);
-      } else {
-        selectedIndexes.add(index);
-      }
-      widget.onChanged(selectedIndexes);
+      selectedIndex = index;
     });
+    widget.onChanged(index);
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        ...List.generate(widget.options.length, (index) {
-          final isSelected = selectedIndexes.contains(index);
+      children: List.generate(widget.options.length, (index) {
+        final isSelected = selectedIndex == index;
 
-          return GestureDetector(
-            onTap: () => toggleOption(index),
-            child: Container(
-              margin: EdgeInsets.symmetric(vertical: 6.h),
-              decoration: BoxDecoration(
-                color: isSelected
-                    ? AppColors.blueShades[10]
-                    : AppColors.lightBlue,
-                borderRadius: BorderRadius.circular(10.r),
-              ),
-              child: Row(
-                children: [
-                  Radio<bool>(
-                    value: true,
-                    groupValue: isSelected,
-                    activeColor: AppColors.blueShades[60],
-                    onChanged: (_) => toggleOption(index),
-                  ),
-
-                  const SizedBox(width: 10),
-
-                  Expanded(
-                    child: Text(
-                      widget.options[index],
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
-                  ),
-                ],
-              ),
+        return GestureDetector(
+          onTap: () => selectOption(index),
+          child: Container(
+            margin: EdgeInsets.symmetric(vertical: 6.h),
+            decoration: BoxDecoration(
+              color: isSelected
+                  ? AppColors.blueShades[10]
+                  : AppColors.lightBlue,
+              borderRadius: BorderRadius.circular(10.r),
             ),
-          );
-        }),
-      ],
+            child: Row(
+              children: [
+                Radio<int>(
+                  value: index,
+                  groupValue: selectedIndex,
+                  activeColor: AppColors.blueShades[60],
+                  onChanged: (_) => selectOption(index),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Text(
+                    widget.options[index],
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }),
     );
   }
 }
