d52e57c680a301619292dc8be126ba6d03b9835e
mohamedqandill <mohamedqandill912@gmail.com>
2025-08-15 21:16:50 +0300

    feat:finish result screen ui

--- lib/core/utils/constatnts.dart
+++ lib/core/utils/constatnts.dart
@@ -4,6 +4,7 @@ abstract class Constants {
   static const String survey = "Survey";
   static const String explore = "Explore";
   static const String result = "Result";
+  static const String results = "Results";
   static const String search = "Search";
   static const String subject = "subject";
   static const String language = "Language";
--- lib/presentation_layer/main_layouts/results/view/result_view.dart
+++ lib/presentation_layer/main_layouts/results/view/result_view.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../exams/widgets/custom_exam_container.dart';
 
 class ResultView extends StatelessWidget {
   const ResultView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Expanded(
+        child: CustomScrollView(
+          slivers: [
+            SliverList.separated(
+              itemCount: 2,
+              itemBuilder: (context, index) {
+                return const InkWell(
+                  // onTap: () => Navigator.pushNamed(
+                  //     context, Routes.quiz,
+                  //     arguments: ""),
+                  child: CustomExamContainer(
+                    quizTitle: "mmmm",
+                    durationNumber: "sdas",
+                    questionNumber: "sad",
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return SizedBox(
+                  height: 10.h,
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
